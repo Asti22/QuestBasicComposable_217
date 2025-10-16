0d9640be8653faf557fd985894754435557b6806
Asti22 <uun3461@gmail.com>
2025-10-16 13:33:33 +0700

    Menampilkan dua baris di dalam satu kolom, masing-masing berisi tiga teks yang tersusun horizontal dengan jarak antar teks merata di seluruh lebar layar.

--- app/src/main/java/com/example/layout/Tataletak.kt
+++ app/src/main/java/com/example/layout/Tataletak.kt
@@ -108,10 +108,18 @@ fun TataletakBoxColumnRow(modifier: Modifier) {
                     modifier = modifier.fillMaxWidth(),
                     horizontalArrangement = Arrangement.SpaceEvenly
                 ){
-                    Text(text = "Col1_Row_Komponen1")
-                    Text(text = "Col1_Row_Komponen2")
-                    Text(text = "Col1_Row_Komponen3")
+                    Text(text = "Col1_Row1_Komponen1")
+                    Text(text = "Col1_Row1_Komponen2")
+                    Text(text = "Col1_Row1_Komponen3")
 
+                }
+                Row(
+                    modifier = modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ){
+                    Text(text = "Col1_Row2_Komponen1")
+                    Text(text = "Col1_Row2_Komponen2")
+                    Text(text = "Col1_Row2_Komponen3")
                 }
             }
         }
